306b8984c27a365f575b3b7a711e710bfe963204
MelgarejoGalianoNick <nick.melgarejo@unmsm.edu.pe>
2023-08-31 13:42:38 -0500

    Agregando la carpeta Model al proyecto

---
+++ app/src/main/java/com/gestion/gestionmantenimientosoftware/Model/Dto.kt
@@ -0,0 +1,36 @@
+package com.gestion.app_despacho.Model
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginDto(
+    @SerializedName("Code")
+    val Code: Int,
+    @SerializedName("Description")
+    val Description: String,
+    @SerializedName("Data")
+    val Data: User
+)
+data class PickingDto(
+    @SerializedName("Code")
+    val Code: Int,
+    @SerializedName("Description")
+    val Description: String,
+    @SerializedName("Data")
+    val Data: Picking
+)
+
+data class MailDto(
+    @SerializedName("Code")
+    val Code: Int,
+    @SerializedName("Description")
+    val Description: String,
+    @SerializedName("Data")
+    val Data: Mail
+)
+
+data class StandardDto(
+    @SerializedName("Code")
+    val Code: Int,
+    @SerializedName("Description")
+    val Description: String
+)
